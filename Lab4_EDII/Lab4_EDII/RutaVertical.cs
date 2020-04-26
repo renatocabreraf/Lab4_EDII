562c8757e0cae94772a10a57ff4ec6599a921a7a
Renato <renatojose98@yahoo.com>
2020-04-26 16:54:20 -0600

    Cifrado mejorados

--- Lab4_EDII/Lab4_EDII/RutaVertical.cs
+++ Lab4_EDII/Lab4_EDII/RutaVertical.cs
@@ -1,277 +1,44 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Lab4_EDII
 {
-    public class Ruta
-    {
-        int x { get; set; }
-        int y { get; set; }
-        string texto { get; set; }
-        string nombreArchivo { get; set; }
-
-        char[,] matrix;
-
-        public Ruta(int m, int n, string texto, string nombreArchivo)
+    public class RutaVertical
         {
-            this.x = x;
-            this.y = y;
-            this.nombreArchivo = nombreArchivo;
-            matrix = new char[m, n];
-            texto = texto.Remove(texto.Length - 1);
-            texto = texto.Remove(texto.Length - 1);
-            this.texto = texto;
-        }
-
-        public void vertical()
-        {
-            int cont = 0;
-            for (int i = 0; i < y; i++)
+            private char[] VerificacionCadena(IFormFile Archivo, int m, int n, char matriz)
             {
-                for (int j = 0; j < x; j++)
+                char[] TextoCompleto = null;
+                char[] Resultante = null;
+                string Temp;
+                using (var Lectura = new StreamReader(Archivo.OpenReadStream()))
                 {
-                    if (cont != texto.Length)
-                        matrix[j, i] = texto[cont];
-                    else
-                        matrix[j, i] = '#';
-                    cont++;
+                    var CapturarArchivo = new StringBuilder();
+                    while (Lectura.Peek() >= 0)
+                    {
+                        CapturarArchivo.AppendLine(Lectura.ReadLine());
+                    }
+                    Temp = CapturarArchivo.ToString();
+                    if (Temp.Contains("\r\n"))
+                    {
+                        Temp = Temp.Replace("\r\n", "\n");
+                    }
+                    TextoCompleto = Temp.ToCharArray();
                 }
+                return Resultante;
             }
-            string outPut = "";
-            for (int i = 0; i < x; i++)
+            //Constructor de clase
+            public RutaVertical()
             {
-                for (int j = 0; j < y; j++)
-                {
-                    outPut += matrix[i, j];
-                }
             }
-            string folder = @"C:\";
-            string fullPath = folder + nombreArchivo;
-            DirectoryInfo directory = Directory.CreateDirectory(folder);
-            using (StreamWriter file = new StreamWriter(fullPath))
-            {
-                file.WriteLine(outPut);
-                file.Close();
-            }
-        }
-        public void decipherVertical()
-        {
-            int cont = 0;
-            for (int i = 0; i < x; i++)
-            {
-                for (int j = 0; j < y; j++)
-                {
-                    matrix[i, j] = texto[cont];
-                    cont++;
-                }
-            }
-            string outPut = "";
-            for (int i = 0; i < y; i++)
-            {
-                for (int j = 0; j < x; j++)
-                {
-                    outPut += matrix[j, i];
-                }
-            }
-            outPut = outPut.Replace('#', ' ');
-            string folder = @"C:\";
-            string fullPath = folder + nombreArchivo;
-            DirectoryInfo directory = Directory.CreateDirectory(folder);
-            using (StreamWriter file = new StreamWriter(fullPath))
-            {
-                file.WriteLine(outPut);
-                file.Close();
-            }
-        }
-        public void spiral()
-        {
-            int cont = 0;
-            bool right = true, down = false, left = false, up = false;
-            int a = 0, b = 0;
-            for (int i = 0; i < x * y; i++)
-            {
-                if (right)
-                {
-                    if (b < y && matrix[a, b] == '\0')
-                    {
-                        matrix[a, b] = (cont < texto.Length) ? texto[cont] : '#';
-                        cont++;
-                        b++;
-                    }
-                    else
-                    {
-                        b--;
-                        a++;
-                        right = false;
-                        down = true;
-                        i--;
-                    }
-                }
-                else if (down)
-                {
-                    if (a < x && matrix[a, b] == '\0')
-                    {
-                        matrix[a, b] = (cont < texto.Length) ? texto[cont] : '#';
-                        cont++;
-                        a++;
-                    }
-                    else
-                    {
-                        a--;
-                        b--;
-                        down = false;
-                        left = true;
-                        i--;
-                    }
-                }
-                else if (left)
-                {
-                    if (b > -1 && matrix[a, b] == '\0')
-                    {
-                        matrix[a, b] = (cont < texto.Length) ? texto[cont] : '#';
-                        cont++;
-                        b--;
-                    }
-                    else
-                    {
-                        b++;
-                        a--;
-                        left = false;
-                        up = true;
-                        i--;
-                    }
-                }
-                else if (up)
-                {
-                    if (a > -1 && matrix[a, b] == '\0')
-                    {
-                        matrix[a, b] = (cont < texto.Length) ? texto[cont] : '#';
-                        cont++;
-                        a--;
-                    }
-                    else
-                    {
-                        b++;
-                        a++;
-                        up = false;
-                        right = true;
-                        i--;
-                    }
-                }
-            }
-            string outPut = "";
-            for (int j = 0; j < y; j++)
-            {
-                for (int i = 0; i < x; i++)
-                {
-                    outPut += matrix[i, j];
-                }
-            } 
-            string folder = @"C:\";
-            string fullPath = folder + nombreArchivo;
-            DirectoryInfo directory = Directory.CreateDirectory(folder);
-            using (StreamWriter file = new StreamWriter(fullPath))
-            {
-                file.WriteLine(outPut);
-                file.Close();
-            }
-        }
-        public void decipherSpiral()
-        {
-            int cont = 0;
-            for (int j = 0; j < y; j++)
-            {
-                for (int i = 0; i < x; i++)
-                {
-                    matrix[i, j] = texto[cont];
-                    cont++;
-                }
-            }
-            string output = "";
-            bool right = true, down = false, left = false, up = false;
-            int a = 0, b = 0;
-            for (int i = 0; i < x * y; i++)
-            {
-                if (right)
-                {
-                    if (b < y && matrix[a, b] != '\0')
-                    {
-                        output += matrix[a, b].ToString();
-                        matrix[a, b] = '\0';
-                        b++;
-                    }
-                    else
-                    {
-                        b--;
-                        a++;
-                        right = false;
-                        down = true;
-                        i--;
-                    }
-                }
-                else if (down)
-                {
-                    if (a < x && matrix[a, b] != '\0')
-                    {
-                        output += matrix[a, b].ToString();
-                        matrix[a, b] = '\0';
-                        a++;
-                    }
-                    else
-                    {
-                        a--;
-                        b--;
-                        down = false;
-                        left = true;
-                        i--;
-                    }
-                }
-                else if (left)
-                {
-                    if (b > -1 && matrix[a, b] != '\0')
-                    {
-                        output += matrix[a, b].ToString();
-                        matrix[a, b] = '\0';
-                        b--;
-                    }
-                    else
-                    {
-                        b++;
-                        a--;
-                        left = false;
-                        up = true;
-                        i--;
-                    }
-                }
-                else if (up)
-                {
-                    if (a > -1 && matrix[a, b] != '\0')
-                    {
-                        output += matrix[a, b].ToString();
-                        matrix[a, b] = '\0';
-                        a--;
-                    }
-                    else
-                    {
-                        b++;
-                        a++;
-                        up = false;
-                        right = true;
-                        i--;
-                    }
-                }
-            }
-            string folder = @"C:\";
-            string fullPath = folder + nombreArchivo;
-            DirectoryInfo directory = Directory.CreateDirectory(folder);
-            using (StreamWriter file = new StreamWriter(fullPath))
-            {
-                file.WriteLine(output);
-                file.Close();
-            }
+            int m { get; set; }
+            int n { get; set; }
+            char[,] matriz { get; set; }
+            public string NuevoNombre { get; set; }
         }
     }
-}
+
--- Lab4_EDII/Lab4_EDII/zigzag.cs
+++ Lab4_EDII/Lab4_EDII/zigzag.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,159 +10,233 @@ namespace Lab4_EDII
 {
     public class zigzag
     {
-        string texto;
-        string nombreArchivo { get; set; }
-        int rails { get; set; }
-        public void calculate(int rails, StringBuilder input, string nombreArchivo)
+        private char[] VerificacionCadena(IFormFile Archivo, int TamañoCarril)
         {
-            this.nombreArchivo = nombreArchivo;
-            this.rails = rails;
-            texto = input.ToString();
-            texto = texto.Remove(texto.Length - 1);
-            texto = texto.Remove(texto.Length - 1);
-            float charQty = 1 + 1 + (2 * (rails - 2));
-            float unit = charQty / (charQty * charQty);
-            float auxLength = texto.Length;
-            float waves = auxLength / charQty;
-            double rest = waves - Math.Truncate(waves);
-
-            rest = 1 - rest;
-            rest = rest / unit;
-
-            for (int i = 0; i < rest; i++)
+            char[] Texto = null;
+            char[] Resultado = null;
+            char[] Borrador = null;
+            string[] Receptor = null;
+            string Temp;
+            double NumeroOla = ((TamañoCarril * 2) - 2);
+            double Ola = 0;
+            int tempNumero = 0;
+            using (var Lectura = new StreamReader(Archivo.OpenReadStream()))
             {
-                texto += "#";
-            }
-
-            fillMatrix(rails, texto);
-
-        }
-        public void fillMatrix(int rails, string texto)
-        {
-            string[,] matrix = new string[rails, texto.Length];
-            int cont = 0;
-            while (cont != texto.Length)
-            {
-                for (int j = 0; j < rails; j++)
+                var TomaArchivo = new StringBuilder();
+                while (Lectura.Peek() >= 0)
                 {
-                    matrix[j, cont] = texto[cont].ToString();
-                    cont++;
+                    TomaArchivo.AppendLine(Lectura.ReadLine());
                 }
-
-                for (int i = rails - 2; i > 0; i--)
+                Temp = TomaArchivo.ToString();
+                Borrador = Temp.ToCharArray();
+                Receptor = new string[Borrador.Length - 2];
+                for (int i = 0; i < Borrador.Length - 2; i++)
                 {
-                    matrix[i, cont] = texto[cont].ToString();
-                    cont++;
+                    Receptor[i] = Borrador[i].ToString();
+                }
+                Temp = String.Concat(Receptor);
+
+                if (Temp.Contains("\r\n"))
+                {
+                    Temp = Temp.Replace("\r\n", "\n");
+                }
+                Texto = Temp.ToCharArray();
+
+            }
+            Ola = Texto.Length / NumeroOla;
+            if (Ola % 1 != 0)
+            {
+                double resulado1 = ((Ola % 1) - 1) * -1;
+                Ola = Ola + resulado1;
+                Resultado = new char[Convert.ToInt32(Ola * NumeroOla)];
+                for (int i = 0; i < Texto.Length; i++)
+                {
+                    Resultado[i] = Texto[i];
+                }
+                tempNumero = ((Convert.ToInt32(Ola * NumeroOla)) - 1);
+                for (int i = tempNumero - 1; i < Ola * NumeroOla; i++)
+                {
+                    Resultado[i] = '#';
                 }
             }
-
-            cipher(matrix);
+            else
+            {
+                Resultado = Texto;
+            }
+            return Resultado;
         }
 
-        public void cipher(string[,] matrix)
+        public void CifradoZigZag(IFormFile Archivo, int TamañoCarril, string NombreArchivo, string path)
         {
-            string encryptedtexto = "";
-
-            for (int i = 0; i < rails; i++)
+            TamañoCarril = 3;
+            char[] ArregloValores = VerificacionCadena(Archivo, TamañoCarril);
+            string[] CadenaCifrada = new string[ArregloValores.Length];
+            int contador = 0;
+            if (TamañoCarril == 3)
             {
-                for (int j = 0; j < texto.Length; j++)
+                for (int i = 1; i <= TamañoCarril; i++)
                 {
-                    if (matrix[i, j] != "")
+                    for (int j = i - 1; j < ArregloValores.Length; j = j + (TamañoCarril + 1))
                     {
-                        encryptedtexto += matrix[i, j];
+                        if (i <= TamañoCarril - 1 && i != (TamañoCarril - TamañoCarril + 1) && i != TamañoCarril)
+                        {
+                            for (int k = j; k < ArregloValores.Length; k = k + 2)
+                            {
+                                CadenaCifrada[contador] = ArregloValores[k].ToString();
+                                contador++;
+                            }
+                            i++;
+                            j = j - 3;
+                        }
+                        else
+                        {
+                            CadenaCifrada[contador] = ArregloValores[j].ToString();
+                            contador++;
+                        }
                     }
                 }
             }
-            string folder = @"C:\";
-            string fullPath = folder + nombreArchivo;
-            DirectoryInfo directory = Directory.CreateDirectory(folder);
-            using (StreamWriter file = new StreamWriter(fullPath))
-            {
-                file.WriteLine(encryptedtexto);
-                file.Close();
-            }
+            EscrituraCifradoZigZag(CadenaCifrada, NombreArchivo, path);
         }
-        public void decipher(int rails, StringBuilder input, string nombreArchivo)
+        private void EscrituraCifradoZigZag(string[] CadenaCifrada, string NombreArchivo, string pathArchivo)
         {
-            this.texto = input.ToString();
-            this.rails = rails;
-            texto = texto.Remove(texto.Length - 1);
-            texto = texto.Remove(texto.Length - 1);
-
-            int charQty = 2 + (2 * (rails - 2));
-            int picos = texto.Length / charQty;
-            string[,] matrix = new string[rails, (texto.Length - (2 * picos)) / (rails - 2)];
-            fillMatrix(texto, picos, ref matrix);
-            string result = "";
-            int cont = 0;
-            int colExt = 0;
-            int colInt = 0;
-            while (cont != texto.Length)
+            var path = Path.Combine(pathArchivo, System.IO.Path.GetFileNameWithoutExtension(NombreArchivo) + ".txt");
+            using (StreamWriter Escritura = new StreamWriter(path))
             {
-                result += matrix[0, colExt];
-                cont++;
-
-                for (int i = 1; i < (rails - 1); i++)
+                foreach (string item in CadenaCifrada)
                 {
-                    result += matrix[i, colInt];
-                    cont++;
+                    Escritura.Write(item);
                 }
-                colInt++;
-
-                result += matrix[rails - 1, colExt];
-                cont++;
-                colExt++;
-
-                for (int i = rails - 2; i > 0; i--)
-                {
-                    result += matrix[i, colInt];
-                    cont++;
-                }
-                colInt++;
+                Escritura.Close();
             }
 
-            bool deleteAllExtra = false;
-            while (!deleteAllExtra)
-            {
-                if (result[result.Length - 1] == '#')
-                {
-                    result = result.Remove(result.Length - 1);
-                }
-                else
-                {
-                    deleteAllExtra = true;
-                }
-            }
-            string folder = @"C:\";
-            string fullPath = folder + nombreArchivo;
-            DirectoryInfo directory = Directory.CreateDirectory(folder);
-
-            using (StreamWriter file = new StreamWriter(fullPath))
-            {
-                file.WriteLine(result);
-                file.Close();
-            }
         }
-        private void fillMatrix(string texto, int picos, ref string[,] matrix)
-        {
-            for (int i = 0; i < picos; i++)
-            {
-                matrix[0, i] = texto[i].ToString(); 
 
-                int pos = (texto.Length - picos) + i;
-                matrix[rails - 1, i] = texto[pos].ToString();
+        public void DescifradoZigZag(IFormFile Archivo, int TamañoCarril, string NombreArchivo, string Path)
+        {
+            TamañoCarril = 3;
+            string Temp = "";
+            char[] Borrador = null;
+            string[] receptor = null;
+            char[] charResultado = null;
+            double CantidadOla = 0;
+            double CantidadElementos = 0;
+            string[] CadenaInicio;
+            string[] CadenaFinal;
+            string[] CadenaMedio;
+
+            using (var Lectura = new StreamReader(Archivo.OpenReadStream()))
+            {
+                var TomaArchivo = new StringBuilder();
+                while (Lectura.Peek() >= 0)
+                {
+                    TomaArchivo.AppendLine(Lectura.ReadLine());
+                }
+                Temp = TomaArchivo.ToString();
+                Borrador = Temp.ToCharArray();
+                receptor = new string[Borrador.Length - 2];
+                for (int i = 0; i < Borrador.Length - 2; i++)
+                {
+                    receptor[i] = Borrador[i].ToString();
+                }
+                Temp = String.Concat(receptor);
+
+                Temp = Temp.Replace("\r\n", "\n");
+                if (Temp.Contains("\r\n"))
+                {
+                    Temp.Replace("\r\n", "\n");
+                }
+                charResultado = Temp.ToCharArray();
+                CantidadElementos = (TamañoCarril * 2) - 2;
+                CantidadOla = charResultado.Length / CantidadElementos;
+                CadenaInicio = new string[Convert.ToInt32(CantidadOla)];
+                CadenaFinal = new string[Convert.ToInt32(CantidadOla)];
+                CadenaMedio = new string[charResultado.Length - (Convert.ToInt32(CantidadOla) * 2)];
+                int aux = 0;
+                for (int i = 0; i < CantidadOla; i++)
+                {
+                    CadenaInicio[i] = charResultado[i].ToString();
+                }
+
+                for (int j = ((charResultado.Length) - Convert.ToInt32(CantidadOla)); j < charResultado.Length; j++)
+                {
+                    CadenaFinal[aux] = charResultado[j].ToString();
+                    aux++;
+                }
+                aux = 0;
+                for (int k = Convert.ToInt32(CantidadOla); k < (charResultado.Length - Convert.ToInt32(CantidadOla)); k++)
+                {
+                    CadenaMedio[aux] = charResultado[k].ToString();
+                    aux++;
+                }
+
+                int ContadorInicio = 0;
+                int ContadorMedio = 0;
+                int ContadorFinal = 0;
+                int ValorContador = 1;
+                int TempContador = 0;
+                string[] CadenaResultado = new string[charResultado.Length];
+                int data = 0;  
+                for (int n = 0; n < charResultado.Length; n = n)
+                {
+
+                    if (ValorContador == 1)
+                    {
+                        CadenaResultado[data] = CadenaInicio[ContadorInicio];
+                        ValorContador++;
+                        n++;
+                        ContadorInicio++;
+                        TempContador = 1;
+                        data++;
+                    }
+                    if (ValorContador == 2)
+                    {
+                        CadenaResultado[data] = CadenaMedio[ContadorMedio];
+                        ValorContador++;
+                        if (TempContador == 3)
+                        {
+                            ValorContador = 1;
+                        }
+                        TempContador = 2;
+                        n++;
+                        ContadorMedio++;
+                        data++;
+                    }
+                    if (ValorContador == 3)
+                    {
+                        CadenaResultado[data] = CadenaFinal[ContadorFinal];
+                        TempContador = 3;
+                        ValorContador--;
+                        n++;
+                        ContadorFinal++;
+                        data++;
+                    }
+                }
+                Lectura.Close();
+                EscrituraDescifradoZigZag(CadenaResultado, NombreArchivo, Path);
             }
 
-            int split = (texto.Length - (picos * 2)) / (rails - 2);
-            int cont = picos;
-            for (int i = 1; i < rails - 1; i++)
+        }
+        private void EscrituraDescifradoZigZag(string[] CadenaResultado, string NombreArchivo, string pathArchivo)
+        {
+            for (int i = 0; i < CadenaResultado.Length; i++)
             {
-                for (int j = 0; j < split; j++)
+                if (CadenaResultado[i] == "#")
                 {
-                    matrix[i, j] = texto[cont].ToString();
-                    cont++;
+                    CadenaResultado[i] = "";
                 }
             }
+            var path = Path.Combine(pathArchivo, System.IO.Path.GetFileNameWithoutExtension(NombreArchivo) + ".txt");
+            using (StreamWriter Escritura = new StreamWriter(path))
+            {
+                foreach (string item in CadenaResultado)
+                {
+                    Escritura.Write(item);
+                }
+                Escritura.Close();
+            }
+
+
         }
     }
 }
