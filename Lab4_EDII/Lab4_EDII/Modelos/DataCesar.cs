41ec00ab939e8e3053ad4781ae84d24856be0bf4
Renato <renatojose98@yahoo.com>
2020-04-26 16:56:31 -0600

    Modelos de Cifrado

---
+++ Lab4_EDII/Lab4_EDII/Modelos/DataCesar.cs
@@ -0,0 +1,16 @@
+﻿using Lab4_EDII.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4_EDII.Modelos
+{
+    public class DataCesar : InterfaceDataCesar<int>
+    {
+        public IFormFile ArchivoCargado { get; set; }
+        public int Llave { get; set; }
+        public string NuevoNombre { get; set; }
+    }
+}
---
+++ Lab4_EDII/Lab4_EDII/Modelos/DataRutaVertical.cs
@@ -0,0 +1,18 @@
+﻿using Lab4_EDII.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4_EDII.Modelos
+{
+    public class DataRutaVertical : InterfaceDataRutaVertical<int>
+    {
+        public IFormFile ArchivoCargado { get; set; }
+        public int m { get; set; }
+        public int n { get; set; }
+        public char[,] matriz { get; set; }
+        public string NuevoNombre { get; set; }
+    }
+}
