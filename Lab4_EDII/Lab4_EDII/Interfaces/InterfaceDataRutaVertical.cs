2166c14db7e76710a00db7d5686e7020e55e31f8
Renato <renatojose98@yahoo.com>
2020-04-26 16:56:54 -0600

    Intercaces de los modelos de cifrado

---
+++ Lab4_EDII/Lab4_EDII/Interfaces/InterfaceDataRutaVertical.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4_EDII.Interfaces
+{
+    public class InterfaceDataRutaVertical<T>
+    {
+        IFormFile ArchivoCargado { get; set; }
+        T m { get; set; }
+        T n { get; set; }
+        T matriz { get; set; }
+        string NuevoNombre { get; set; }
+    }
+}
---
+++ Lab4_EDII/Lab4_EDII/Interfaces/InterfaceDataZigZag.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4_EDII.Interfaces
+{
+    public interface InterfaceDataZigZag<T>
+    {
+        IFormFile ArchivoCargado { get; set; }
+        T TamañoCarriles { get; set; }
+        string NuevoNombre { get; set; }
+
+    }
+}
